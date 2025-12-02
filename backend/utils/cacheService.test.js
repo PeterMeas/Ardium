b6746aca9ccbdb9683992d92a1fa4c935a3b4e8d
PeterMeas <pmeas145@gmail.com>
2025-12-02 01:27:38 -0500

    test: add setCachedStock test and fix Jest config
    - Added unit test for setCachedStock method in cacheService
    - Configured Jest to ignore node_modules tests
    - Test verifies stockCache.set is called with correct key, data, and TTL
    - All 11 tests passing

--- backend/utils/cacheService.test.js
+++ backend/utils/cacheService.test.js
@@ -11,7 +11,6 @@ SimpleCache.mockImplementation(() => ({
 }));
 
 describe('cacheService', () => {
-    
 
     describe('getCachedStock', () => {
         test('should call stockCache with correct key', () => {
@@ -20,6 +19,17 @@ describe('cacheService', () => {
             const result = cacheService.getCachedStock('AAPL', 'compact');
             expect(SimpleCache.mock.instances[1].get).toHaveBeenCalledWith('AAPL::compact');
             expect(result).toEqual(fakeData);
+        })
+    })
+
+    describe('setCachedStock', () => {
+        test('should verify stockCache.set is called', () => {
+            const fakeData = {
+                symbol: 'AAPL',
+                data: 'compact'
+            }
+            cacheService.setCachedStock('AAPL', 'compact', fakeData, 100 )
+            expect(SimpleCache.mock.instances[1].set).toHaveBeenCalledWith('AAPL::compact', fakeData, 100)
         })
     })
 })
